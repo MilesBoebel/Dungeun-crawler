69a9f36df5d4e3debf1c42a59a135e5b3fd0cbf5
FortView <117851972+MilesBoebel@users.noreply.github.com>
2023-01-25 16:25:41 -0600

    w

---
+++ FPS3dGame/Assets/Scripts/InputManager.cs
@@ -0,0 +1,50 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+    private PlayerMotor motor;
+    private PlayerLook look;
+
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+
+        motor = GetComponent<PlayerMotor>();
+        onFoot.Jump.performed += ctx => motor.Jump();
+
+        look = GetComponent<PlayerLook>();
+
+        onFoot.Crouch.performed += ctx => motor.Crouch();
+        onFoot.Sprint.performed += ctx => motor.Sprint();
+    }
+
+    void FixedUpdate()
+    {
+        //tell the playermotor to move using the value from out movement action.
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+    }
+
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+
+}
