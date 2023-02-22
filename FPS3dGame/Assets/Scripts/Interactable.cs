9e2cc475f597127110ce7b103f82ca9dbbbd3094
FortView <117851972+MilesBoebel@users.noreply.github.com>
2023-02-21 22:01:01 -0600

    Player Health System and working Health Bar UI display and 2 cubes that heal and damge the player when you interact with them

--- FPS3dGame/Assets/Scripts/Interactable.cs
+++ FPS3dGame/Assets/Scripts/Interactable.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    //Add or remvoe InteractioEvent component to this gameObject
+    public bool useEvents;
     //Message displayed to player when looking at an interatable
     public string promptMessage;
 
     //This function will be called from our player
     public void BaseInteract()
     {
+        if(useEvents)
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
         Interact();
     }
 
